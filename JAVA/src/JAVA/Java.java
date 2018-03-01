33ceb55acf756a725cca08ad9776e17d63753eaf
nelsinhowbn <nelsonhadi95@gmail.com>
2018-03-01 20:52:41 -0300

    exercicios de java

---
+++ JAVA/src/JAVA/Java.java
@@ -0,0 +1,37 @@
+package JAVA;
+
+public class Java { public static void main( String[] args){
+	double a, r, PI;
+	r=12;
+	PI= 3.1415;
+	a= PI*Math.pow(r, 2);
+	System.out.println("Á árera será: "+ a);
+	
+   int resto = 99%4;
+   System.out.println(" O resto da divisão será : "+ resto);
+   
+   
+   int n=10;
+   double resultado;
+   resultado= n*0.5; 
+   System.out.println(" O resultado da divisão por 2 será: " + resultado);
+   
+	
+   int n=20;
+   double resultado;
+   resultado= 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
